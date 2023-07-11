3b901592a8b71f52558254b8872c15585ecfe4e8
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-12 04:00:07 +0530

    Add filters

--- app/lib/queries/index.ts
+++ app/lib/queries/index.ts
@@ -27,16 +27,91 @@ export async function getUsersWithLikedPostsStartingWithJ() {
 }
 //GET all users whose IDs are not in the range of 1 to 2
 export async function getUsersNotInRange() {
-    const users = await prisma.user.findMany({
-        where: {
-            id: {
-                notIn: [1, 2],
-            },
-        },
-    });
-    return users;
+  const users = await prisma.user.findMany({
+    where: {
+      id: {
+        notIn: [1, 2],
+      },
+    },
+  });
+  return users;
 }
 
-export async function getUsers() {
-  
+//Id not greater than 2 and the name starts with S/s
+export async function getUsersStartingWithSOrWithIdGreaterThanTwo() {
+  const users = await prisma.user.findMany({
+    where: {
+      id: {
+        gt: 2,
+      },
+      OR: {
+        name: {
+          startsWith: "S",
+          mode: "insensitive",
+        },
+      },
+    },
+  });
+  const alternateImplementation = await prisma.user.findMany({
+    where: {
+      OR: [
+        {
+          id: {
+            gt: 2,
+          },
+        },
+        {
+          name: {
+            startsWith: "S",
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
+
+  return users;
 }
+
+// All published posts with github and twitter in the title
+export async function getPosts() {
+  return await prisma.post.findMany({
+    where: {
+      AND: [
+        {
+          OR: [
+            {
+              title: {
+                contains: "github",
+                mode: "insensitive",
+              },
+            },
+            {
+              title: {
+                contains: "twitter",
+                mode: "insensitive",
+              },
+            },
+          ],
+        },
+        {
+          published: true,
+        },
+      ],
+    },
+  });
+}
+
+
+
+export async function getUsers() {
+  return await prisma.user.findMany();
+}
+
+// export async function getUsers() {
+//   return await prisma.user.findMany();
+// }
+
+// export async function getPosts() {
+//   return await prisma.posts.findMany();
+// }
