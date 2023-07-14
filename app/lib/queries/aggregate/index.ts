f0345863f509ac8a319e7bf34510e73dfa3c9ddc
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-07-15 00:16:48 +0530

    LO: Aggregate functions

--- app/lib/queries/aggregate/index.ts
+++ app/lib/queries/aggregate/index.ts
@@ -1,6 +1,35 @@
 import prisma from "../../prisma";
 
-export async function getPosts() {
-  const posts = await prisma.post.findFirst();
+export async function getLikeCountOfPublishedPosts() {
+  const posts = await prisma.post.aggregate({
+    where: {
+      published: true,
+    },
+    _sum: {
+      likeNum: true,
+    },
+  });
   return posts;
+}
+
+// Avg like count of the posts in the Big Data category
+export async function getPosts() {
+  return await prisma.post.aggregate({
+    where: {
+      categories: {
+        some: {
+          name: "Big Data",
+        },
+      },
+    },
+    _avg: {
+      likeNum: true,
+    },
+    _sum: {
+      likeNum: true,
+    },
+    _count: {
+      published: true,
+    },
+  });
 }
